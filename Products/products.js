9dd382e6dab8d20a0cbf947680ebaf09f5936da2
Aarav Gaudana <aaravgaudana7@gmail.com>
2025-08-29 09:30:38 -0400

    in class changes

--- Products/products.js
+++ Products/products.js
@@ -8,54 +8,6 @@ womens.addEventListener("change", filtering);
 childrens.addEventListener("change", filtering);
 unisex.addEventListener("change", filtering);
 
-function toggleMens() {
-    if(mens.checked) {
-        document.querySelectorAll(".womens-apparel, .unisex-apparel, .childrens-apparel").forEach(element => {
-            element.style.display = "none";
-        });
-    } else {
-        document.querySelectorAll(".womens-apparel, .unisex-apparel, .childrens-apparel").forEach(element => {
-            element.style.display = "";
-        });
-    }
-}
-
-function toggleWomens() {
-    if(womens.checked) {
-        document.querySelectorAll(".mens-apparel, .unisex-apparel, .childrens-apparel").forEach(element => {
-            element.style.display = "none";
-        });
-    } else {
-        document.querySelectorAll(".mens-apparel, .unisex-apparel, .childrens-apparel").forEach(element => {
-            element.style.display = "";
-        });
-    }
-}
-
-function toggleChildrens() {
-    if(childrens.checked) {
-        document.querySelectorAll(".womens-apparel, .mens-apparel, .unisex-apparel").forEach(element => {
-            element.style.display = "none";
-        });
-    } else {
-        document.querySelectorAll(".womens-apparel, .mens-apparel, .unisex-apparel").forEach(element => {
-            element.style.display = "";
-        });
-    }
-}
-
-function toggleUnisex() {
-    if(unisex.checked) {
-        document.querySelectorAll(".womens-apparel, .mens-apparel, .childrens-apparel").forEach(element => {
-            element.style.display = "none";
-        });
-    } else {
-        document.querySelectorAll(".womens-apparel, .mens-apparel, .childrens-apparel").forEach(element => {
-            element.style.display = "";
-        });
-    }
-}
-
 function filtering() {
     document.querySelectorAll(".mens-apparel, .womens-apparel, .childrens-apparel, .unisex-apparel").forEach(element => {
         element.style.display = "none";
