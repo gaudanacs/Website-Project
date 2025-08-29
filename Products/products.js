808a341c4a526d67dacced341ca71199d3017e61
Aarav <aaravgaudana7@gmail.com>
2025-08-29 00:02:24 -0400

    revamped filtering

--- Products/products.js
+++ Products/products.js
@@ -3,10 +3,10 @@ const womens = document.getElementById("womens");
 const childrens = document.getElementById("childrens");
 const unisex = document.getElementById("unisex");
 
-mens.addEventListener("change", toggleMens);
-womens.addEventListener("change", toggleWomens);
-childrens.addEventListener("change", toggleChildrens);
-unisex.addEventListener("change", toggleUnisex);
+mens.addEventListener("change", filtering);
+womens.addEventListener("change", filtering);
+childrens.addEventListener("change", filtering);
+unisex.addEventListener("change", filtering);
 
 function toggleMens() {
     if(mens.checked) {
@@ -51,6 +51,38 @@ function toggleUnisex() {
         });
     } else {
         document.querySelectorAll(".womens-apparel, .mens-apparel, .childrens-apparel").forEach(element => {
+            element.style.display = "";
+        });
+    }
+}
+
+function filtering() {
+    document.querySelectorAll(".mens-apparel, .womens-apparel, .childrens-apparel, .unisex-apparel").forEach(element => {
+        element.style.display = "none";
+    });
+
+    const selectedBox = document.querySelector("input[name='filter']:checked").id;
+
+    if (selectedBox === "mens") {
+        document.querySelectorAll(".mens-apparel").forEach(element => {
+            element.style.display = "";
+        });
+    }
+
+    if (selectedBox === "womens") {
+        document.querySelectorAll(".womens-apparel").forEach(element => {
+            element.style.display = "";
+        });
+    }
+
+    if (selectedBox === "unisex") {
+        document.querySelectorAll(".unisex-apparel").forEach(element => {
+            element.style.display = "";
+        });
+    }
+
+    if (selectedBox === "childrens") {
+        document.querySelectorAll(".childrens-apparel").forEach(element => {
             element.style.display = "";
         });
     }
