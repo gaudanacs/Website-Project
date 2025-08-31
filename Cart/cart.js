a148de9c1843c4c54df8189d1f5de28b3f2363ff
Aarav <aaravgaudana7@gmail.com>
2025-08-31 16:24:54 -0400

    cookie test

---
+++ Cart/cart.js
@@ -0,0 +1,23 @@
+function viewcart() {
+    let cart=[];
+
+    if (document.cookie) {
+        cart = JSON.parse(document.cookie.split("=")[1]);
+    }
+
+    if (cart.length === 0) {
+        alert("Your cart is empty");
+    } else {
+        let message = "";
+        for (let i=0; i<cart.length; i++) {
+            const item = cart[i];
+            message += `Product: ${item.product}, Size: ${item.itemsize}, Quantity: ${item.amount}\n`;
+        }
+        alert(message);
+    }
+}
+
+function clearCart() {
+    document.cookie = "cart=; expires=Thu, 18 Dec 2013 12:00:00 UTC; path=/";
+    alert("cart is cleared");
+}
---
+++ Products/ProductPages/productpage.js
@@ -0,0 +1,29 @@
+function addToCart(productName) {
+    const selectedSize = document.querySelector("input[name='size']:checked");
+    const quantityDropdown = document.getElementById('quantity');
+
+    let size;
+    let quantity = quantityDropdown.value;
+
+    if (selectedSize) {
+        size=selectedSize.id;
+    } else {
+        size="N/A";
+    };
+
+    const cartItem = {
+        product: productName,
+        itemsize: size,
+        amount: quantity
+    };
+
+    let cart=[];
+
+    if (document.cookie) {
+        cart = JSON.parse(document.cookie.split("=")[1]);
+    }
+
+    cart.push(cartItem);
+
+    document.cookie = "cart=" + JSON.stringify(cart) + "; path=/;";
+}
